fd9daa0a0a16a90bcd80ea9c67bd9229b01529ca
Ryan <ryan.astralis@gmail.com>
2020-10-14 16:51:04 -0300

    Initial Commit
    First commit of GEHelveHammer

---
+++ src/GECore.cs
@@ -0,0 +1,53 @@
+﻿using gehelvehammer.src.behaviors;
+using gehelvehammer.src.blocktypes;
+using gehelvehammer.src.entities;
+using gehelvehammer.src.itemtypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.Util;
+
+namespace gehelvehammer.src
+{
+    class GECore : ModSystem
+    {
+
+        public override void Start(ICoreAPI api)
+        {
+            api.World.Logger.Debug("Installing GE Helvehammer");
+            try
+            {
+                api.RegisterItemClass("GEItemHammer", typeof(GEItemHammer));
+
+                api.RegisterBlockClass("GEBlockAnvil", typeof(GEBlockAnvil));
+                api.RegisterBlockClass("GEBlockToggle", typeof(GEBlockToggle));
+                api.RegisterBlockClass("GEBlockHelveHammer", typeof(GEBlockHelveHammer));
+
+
+                api.RegisterBlockEntityClass("GEAnvil", typeof(GEBEAnvil));
+                api.RegisterBlockEntityClass("GEBEHelveHammer", typeof(GEBEHelveHammer));
+
+                api.RegisterBlockEntityBehaviorClass("GEBEBehaviorMPToggle", typeof(GEBEBehaviorMPToggle));
+
+            }
+            catch (Exception e)
+            {
+                api.World.Logger.Log(EnumLogType.Debug, $"error registering GE Helvehammer: {e.StackTrace}");
+                throw;
+            }
+
+            
+
+        }
+    }
+
+    
+
+
+
+}
---
+++ src/blocktypes/GEBlockToggle.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent.Mechanics;
+
+namespace gehelvehammer.src.blocktypes
+{
+    public class GEBlockToggle : BlockMPBase
+    {
+        public bool IsOrientedTo(BlockFacing facing)
+        {
+            string dirs = LastCodePart();
+
+            return dirs[0] == facing.Code[0] || (dirs.Length > 1 && dirs[1] == facing.Code[0]);
+        }
+
+
+        public override bool HasMechPowerConnectorAt(IWorldAccessor world, BlockPos pos, BlockFacing face)
+        {
+            return IsOrientedTo(face);
+        }
+
+        public override bool TryPlaceBlock(IWorldAccessor world, IPlayer byPlayer, ItemStack itemstack, BlockSelection blockSel, ref string failureCode)
+        {
+            if (!CanPlaceBlock(world, byPlayer, blockSel, ref failureCode))
+            {
+                return false;
+            }
+
+            foreach (BlockFacing face in BlockFacing.HORIZONTALS)
+            {
+                BlockPos pos = blockSel.Position.AddCopy(face);
+
+                IMechanicalPowerBlock block = world.BlockAccessor.GetBlock(pos) as IMechanicalPowerBlock;
+                if (block != null)
+                {
+                    if (block.HasMechPowerConnectorAt(world, pos, face.GetOpposite()))
+                    {
+                        AssetLocation loc = new AssetLocation(FirstCodePart() + "-" + face.GetOpposite().Code[0] + face.Code[0]);
+                        Block toPlaceBlock = world.GetBlock(loc);
+                        if (toPlaceBlock == null)
+                        {
+                            loc = new AssetLocation(FirstCodePart() + "-" + face.Code[0] + face.GetOpposite().Code[0]);
+                            toPlaceBlock = world.GetBlock(loc);
+                        }
+
+                        if (toPlaceBlock.DoPlaceBlock(world, byPlayer, blockSel, itemstack))
+                        {
+                            block.DidConnectAt(world, pos, face.GetOpposite());
+                            WasPlaced(world, blockSel.Position, face);
+                            return true;
+                        }
+                    }
+                }
+            }
+
+
+            if (base.TryPlaceBlock(world, byPlayer, itemstack, blockSel, ref failureCode))
+            {
+                WasPlaced(world, blockSel.Position, null);
+                return true;
+            }
+            return false;
+        }
+
+
+        public override void OnNeighbourBlockChange(IWorldAccessor world, BlockPos pos, BlockPos neibpos)
+        {
+            BEBehaviorMPToggle bemptoggle = world.BlockAccessor.GetBlockEntity(pos)?.GetBehavior<BEBehaviorMPToggle>();
+            if (bemptoggle != null && !bemptoggle.IsAttachedToBlock())
+            {
+                foreach (BlockFacing face in BlockFacing.HORIZONTALS)
+                {
+                    BlockPos npos = pos.AddCopy(face);
+                    BlockAngledGears blockagears = world.BlockAccessor.GetBlock(npos) as BlockAngledGears;
+                    if (blockagears == null) continue;
+                    if (blockagears.Facings.Contains(face.GetOpposite()) && blockagears.Facings.Length == 1)
+                    {
+                        world.BlockAccessor.BreakBlock(npos, null);
+                    }
+                }
+            }
+
+            base.OnNeighbourBlockChange(world, pos, neibpos);
+        }
+
+
+        public override void DidConnectAt(IWorldAccessor world, BlockPos pos, BlockFacing face)
+        {
+
+        }
+    }
+}
